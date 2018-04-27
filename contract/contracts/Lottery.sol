de3cccbc2ca23f07e7bda28eb3e476596aa4476b
sarovin <sarovin86@gmail.com>
2018-04-27 19:11:21 +0200

    FIX solhint

--- contract/contracts/Lottery.sol
+++ contract/contracts/Lottery.sol
@@ -1,35 +1,36 @@
-pragma solidity ^0.4.17;
+pragma solidity 0.4.17;
+
 
 contract Lottery {
 
     address public manager;
     address[] public players;
 
-    function Lottery() public {
-      manager = msg.sender;
+    function constuctor() public {
+        manager = msg.sender;
     }
 
     function enter() public payable {
-      require (msg.value > 0.01 ether);
-      players.push(msg.sender);
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
     }
 
-    function random() private view returns (uint){
-      return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
     }
 
-    function pickWinner() public restricted{
-      uint index = random() % players.length;
-      players[index].transfer(this.balance);
-      players = new address[](0);
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address[](0);
     }
 
     modifier restricted() {
-      require(msg.sender == manager);
-      _;
+        require(msg.sender == manager);
+        _;
     }
 
-    function getPlayers() public view returns (address[]){
-      return players;
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 }

7a74ed1e3ca54ffa87631396653113ac6a1b157d
futuringco <sebastiano@wearefuturing.com>
2018-04-27 09:48:54 +0200

    first commit

---
+++ lottery-react/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+		state = {
+				manager: '',
+				players: [],
+				address: '',
+				balance: '',
+				value: '0.2',
+				message: ''
+		};
+
+		async componentDidMount() {
+					const manager = await lottery.methods.manager().call();
+					const address = lottery.options.address;
+					const players = await lottery.methods.getPlayers().call();
+					const balance = await web3.eth.getBalance(lottery.options.address);
+					this.setState({ manager, players, address, balance });
+		}
+
+		onSubmit = async (event) =>{
+			 event.preventDefault();
+
+				const accounts = await web3.eth.getAccounts();
+
+				this.setState({ message: 'Waiting for Ethereum Network'});
+
+				await lottery.methods.enter().send({
+						from: accounts[0],
+						value: web3.utils.toWei(this.state.value, 'ether')
+				});
+
+				this.setState({ message: 'You\'ve been entered with '+this.state.value+' ETH!'});
+
+				const balance = await web3.eth.getBalance(lottery.options.address);
+				this.setState({ balance });
+		};
+
+  render() {
+			 /*web3.eth.getAccounts()
+						.then(console.log);*/ // visualizza gli account della versione di web3 di Metamask
+    return (
+      <div className="App">
+        <header className="App-header">
+										<img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">
+												Welcome to HACK Lottery
+											</h1>
+        </header>
+        <p className="App-intro">
+									<br></br> deployed by {this.state.manager}
+									<br></br> on <a href="https://etherscan.io/address/{this.state.address}" target="_blank">{this.state.address}</a>
+									<br></br> and has {web3.utils.fromWei(this.state.balance, 'ether')} ETH
+        </p>
+
+								<hr />
+
+							 <form onSubmit={this.onSubmit}>
+										<h4>Enter Lottery</h4>
+										<div>
+												<label>Amount of ether to enter, min is 0,01 ETH</label><br/>
+												<input
+															min="0.01"
+															step="0.01"
+															type="number"
+													  value={this.state.value}
+															onChange={event => this.setState({ value: event.target.value })}
+													/>
+												ETH<br/>
+												<button>Enter</button>
+												<br/><br/>
+												{this.state.message}
+										</div>
+								</form>
+      </div>
+    );
+  }
+}
+
+export default App;
